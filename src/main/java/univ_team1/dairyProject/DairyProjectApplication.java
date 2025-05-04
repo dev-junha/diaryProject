3e56f8b5a1fb9bdd1dab17cbbfaf2c2c96a58bd7
zeehyee <amber9324@naver.com>
2025-05-05 04:24:19 +0900

    기본셋팅

---
+++ src/main/java/univ_team1/dairyProject/DairyProjectApplication.java
@@ -0,0 +1,13 @@
+package univ_team1.dairyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/univ_team1/dairyProject/DairyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package univ_team1.dairyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
