107fa55b17b0de275cb72c91b5be780c6399e18b
zeehyee <amber9324@naver.com>
2025-05-05 17:32:40 +0900

    feat: 일기 생성 API

--- src/main/java/univ_team1/dairyProject/DairyProjectApplication.java
+++ src/main/java/univ_team1/dairyProject/DairyProjectApplication.java
@@ -2,7 +2,9 @@ package univ_team1.dairyProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DairyProjectApplication {
 
